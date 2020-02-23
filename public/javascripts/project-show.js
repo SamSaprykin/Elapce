ba131cd1a47c5c0d3e990b94fdfe7ce29a4639f9
Sam <Saprykinse@gmail.com>
2020-02-23 11:38:09 +0300

    timeout mapbox

--- public/javascripts/project-show.js
+++ public/javascripts/project-show.js
@@ -1,24 +1,26 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZWxhcGNlIiwiYSI6ImNrMnVrMXNjbjExOTQzbW56amloa3FvZTYifQ.xZG1tMykgGuNCvr-EI00xQ';
 
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: project.geometry.coordinates,
-    zoom: 12
-});
-
-
-// create a HTML element for our project
-var el = document.createElement('div');
-el.className = 'marker';
-
-// make a marker for each feature and add to the map
-new mapboxgl.Marker(el)
-.setLngLat(project.geometry.coordinates)
-.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-.setHTML('<h3>' + project.title + '</h3><p>' + project.location + '</p>'))
-.addTo(map);
+setTimeout(function(){
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: project.geometry.coordinates,
+        zoom: 12
+    });
+    
+    
+    // create a HTML element for our project
+    var el = document.createElement('div');
+    el.className = 'marker';
+    
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+    .setLngLat(project.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + project.title + '</h3><p>' + project.location + '</p>'))
+    .addTo(map);
+    
+},2000)
 
 
 
