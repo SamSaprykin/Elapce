5fc5bad264c41b7de9fcaf3116e947f5eca963c1
User <user@MacBook-Air-User.local>
2020-06-25 17:12:01 +0300

    about us goals

--- controllers/about.js
+++ controllers/about.js
@@ -1,4 +1,4 @@
-const Article = require('../models/article');
+const AboutGoals = require('../models/about-goals');
 const User = require('../models/user');
 const { cloudinary } = require('../cloudinary');
 
@@ -6,8 +6,37 @@ module.exports = {
 	// Posts Index
 	async aboutIndex(req, res, next) {
 		const teamMembers = await User.find({ isTeamMember: { $eq: true } });
-		res.render('about/about', { teamMembers, title: 'О нас', image: cloudinary.image, image_url: cloudinary.url });
+		const aboutGoalsData = await AboutGoals.find({})
+		res.render('about/about', { teamMembers,aboutGoalsData, title: 'О нас', image: cloudinary.image, image_url: cloudinary.url });
 	},
+	aboutGoalsNew(req, res, next) {
+		res.render('about/new-goals');
+	},
+	
+	async aboutGoalsCreate(req, res, next) {
+        // use req.body to create a new Article
+        
+        
+        req.body.iconImage = [];
+        
+        for(const file of req.files) {
+			
+			req.body.iconImage.push({
+				url: file.secure_url,
+				public_id: file.public_id
+			});
+        }
+
+		console.log(req.body)
+        
+        text = req.body.body;
+        html = converter.makeHtml(text);
+        req.body.body = html
+        let aboutGoal = new AboutGoals(req.body);
+        await aboutGoal.save();
+        res.redirect(`/about`);
+        
+    },
 	
 	aboutHistory(req, res, next) {
 		res.render('about/history');
--- routes/about.js
+++ routes/about.js
@@ -2,19 +2,27 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { storage } = require('../cloudinary');
-const { asyncErrorHandler, isLoggedIn, searchAndFilterArticles, isTeamMember  } = require('../middleware/index');
+const upload = multer({ storage });
+const { asyncErrorHandler, isLoggedIn, searchAndFilterArticles, isTeamMember, isAdmin  } = require('../middleware/index');
 
 const {
     aboutIndex,
     aboutHistory,
     aboutTeam,
     aboutPress,
+    aboutGoalsNew,
+    aboutGoalsCreate
 } = require('../controllers/about')
 
 
 /* Get articles index /articles */
 
 router.get('/',asyncErrorHandler(aboutIndex));
+
+/* Get guides new /guide */  
+router.get('/new-goals', isLoggedIn,asyncErrorHandler(isAdmin), aboutGoalsNew);
+
+router.post('/new-goals',isLoggedIn,upload.array('iconImage', 1),asyncErrorHandler(isAdmin),  asyncErrorHandler(aboutGoalsCreate));
 
 router.get('/history', asyncErrorHandler(aboutHistory));
 
